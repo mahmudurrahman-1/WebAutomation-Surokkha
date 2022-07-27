3a125e4e5083d0c7c925611abfe4db3fdac19716
mahmudurrahman-1 <rthe1761@gmail.com>
2022-07-27 22:04:35 +0600

    dataset added

--- src/test/java/dataSet/SQL.java
+++ src/test/java/dataSet/SQL.java
@@ -1,4 +1,18 @@
 package dataSet;
 
+import org.testng.annotations.DataProvider;
+
 public class SQL {
+    @DataProvider(name = "registrationdata")
+    public static Object[][] registrationInfos() {
+        Object[][] data = {{"1", "5104961577", "29", 10, 6}};
+        return data;
+    }
+
+    @DataProvider(name = "verifydata")
+    public static Object[][] verifyInfos() {
+        Object[][] data = {{"1", "5104961577", "28", "9", "1999"}};
+        return data;
+    }
+
 }
